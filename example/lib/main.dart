72d254a4a39793a386bf5dbde7702bc319f35b87
Chamsol Kim <kcsol1005@gmail.com>
2024-06-18 21:03:03 +0900

    Refactor example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,39 +11,60 @@ class MyApp extends StatelessWidget {
     const title = 'Customizable Text Sample';
     return const MaterialApp(
       title: title,
-      home: ExampleApp(title: title),
+      home: ExampleApp(),
     );
   }
 }
 
 class ExampleApp extends StatelessWidget {
-  const ExampleApp({super.key, required this.title});
-
-  final String title;
+  const ExampleApp({super.key});
+  final originalText =
+      'By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use. We may use your contact information, including your email address and phone number for purposes outlined in our Privacy Policy, like keeping your account secure and personalizing our services, including ads. Learn more. Others will be able to find you by email or phone number, when provided, unless you choose otherwise here.';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: const Text("Customizable Text Sample"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          children: [
-            _sample1(),
-            const SizedBox(height: 16),
-            _sample2(),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _sample1(),
+              _sample2(),
+              _sample3(),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  CustomizableText _sample1() {
-    const originalText =
-        'By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use. We may use your contact information, including your email address and phone number for purposes outlined in our Privacy Policy, like keeping your account secure and personalizing our services, including ads. Learn more. Others will be able to find you by email or phone number, when provided, unless you choose otherwise here.';
+  Widget _sample1() {
+    return CustomizableText(
+      originalText,
+      style: const TextStyle(
+        color: Colors.grey,
+      ),
+      customStyle: const TextStyle(
+        color: Colors.blue,
+        fontWeight: FontWeight.w500,
+      ),
+      customizes: const [
+        CustomText('Terms of Service'),
+        CustomText('Privacy Policy'),
+        CustomText('Cookie Use'),
+        CustomText('Privacy Policy'),
+        CustomText('Learn more'),
+        CustomText('here'),
+      ],
+    );
+  }
 
+  CustomizableText _sample2() {
     return CustomizableText(
       originalText,
       style: const TextStyle(
@@ -97,20 +118,17 @@ class ExampleApp extends StatelessWidget {
     );
   }
 
-  CustomizableText _sample2() {
-    const originalText =
-        'By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use. We may use your contact information, including your email address and phone number for purposes outlined in our Privacy Policy, like keeping your account secure and personalizing our services, including ads. Learn more. Others will be able to find you by email or phone number, when provided, unless you choose otherwise here.';
-
-    return const CustomizableText(
+  CustomizableText _sample3() {
+    return CustomizableText(
       originalText,
-      style: TextStyle(
+      style: const TextStyle(
         color: Colors.grey,
       ),
-      customStyle: TextStyle(
+      customStyle: const TextStyle(
         color: Colors.blue,
         fontWeight: FontWeight.w500,
       ),
-      customizes: [
+      customizes: const [
         CustomText(
           'Privacy Policy',
           span: 1,
